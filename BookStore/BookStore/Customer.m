9006dfb0eb568e15da3f7baede508a0063644e08
Tejas Mehta <tj@tejas-mehta.com>
2020-05-09 19:43:14 -0700

    Customer class, header file props and methods

---
+++ BookStore/BookStore/Customer.m
@@ -0,0 +1,13 @@
+//
+//  Customer.m
+//  BookStore
+//
+//  Created by Tejas Mehta on 5/9/20.
+//  Copyright © 2020 Tejas Mehta. All rights reserved.
+//
+
+#import "Customer.h"
+
+@implementation Customer
+
+@end
